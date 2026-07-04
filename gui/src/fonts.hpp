ef06bf53e0422d102674dda97799eb84a09e8ce1
Sgt. Matthew <mahdi.sartipzadeh@gmail.com>
2026-07-04 00:07:47 -0700

    feature[]: add gui application

---
+++ gui/src/fonts.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "imgui.h"
+
+namespace fonts {
+	extern ImFont* Roboto_Regular_Small;
+	extern ImFont* Roboto_Regular_Large;
+	extern ImFont* Roboto_Regular_XLarge;
+	extern ImFont* Roboto_Bold_small;
+	extern ImFont* Roboto_Bold_Small;
+	extern ImFont* Roboto_Bold_Large;
+	extern ImFont* Roboto_Bold_XLarge;
+}
